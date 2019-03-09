819832a95226b000f93783c6bcbcf3411c2eaf57
Peter De Rijk <Peter.DeRijk@gmail.com>
2019-03-09 10:02:59 +0100

    updated make and build (using start_hbb.sh)

--- build/hbb_make_zstdra.sh
+++ build/hbb_make_zstdra.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# This script builds zstdra and zstdindex using the Holy build box environment
-# If you give the parameter 32, a 32bit build will be made
-# It expects a build zstd library in zstd-ori/zstd-1.3.8, which can be made using the 
-# hbb_make_zstd.hs script
+# This script builds the zstdra and zstdindex binaries using the Holy build box environment
+# options:
+# -b|-bits|--bits: 32 for 32 bits build (default 64)
+# -d|-builddir|--builddir: top directory to build external software in (default ~/build/bin-$arch)
+# -c|-clean|--clean: 1 to do "make clean" before building (default), use 0 to not run make clean
 
 # The Holy build box environment requires docker, make sure it is installed
 # e.g. on ubuntu and derivatives
@@ -11,43 +12,79 @@
 # Also make sure you have permission to use docker
 # sudo usermod -a -G docker $USER
 
+## If it does not find the required zstd libraries in builddir, it will make it
+
 # stop on error
 set -e
 
-# check if we are already in holy build box
-if [ ! -f /hbb_exe/activate ]; then
-	# find directory of script
-	script="$(readlink -f "$0")"
-	dir="$(dirname "$script")"
-	file="$(basename "$script")"
-	echo "Running script $dir/$file"
-	# we go one dir below (script is in build dir)
-	cd $dir/..
-	# run the script in holy build box
-	if [ "$1" = "32" ]
-	then
-		docker run -t -i --rm -v `pwd`:/io phusion/holy-build-box-32:latest linux32 bash /io/build/$file '-ix86'
-	else
-		docker run -t -i --rm -v `pwd`:/io phusion/holy-build-box-64:latest bash /io/build/$file
-	fi
-	exit
-fi
+# Prepare and start docker with Holy Build box
+# ============================================
+
+script="$(readlink -f "$0")"
+dir="$(dirname "$script")"
+source "${dir}/start_hbb.sh"
+
+# Parse arguments
+# ===============
+
+clean=1
+while [[ "$#" -gt 0 ]]; do case $1 in
+	-c|-clean|--clean) clean="$2"; shift;;
+	*) echo "Unknown parameter: $1"; exit 1;;
+esac; shift; done
+
+# Script run within Holy Build box
+# ================================
 
 echo "Entering Holy Build Box environment"
 
 # Activate Holy Build Box environment.
+# Tk does not compile with these settings (X)
+# only use HBB for glibc compat, not static libs
 source /hbb_exe/activate
-
-export ARCH="$1"
-
-echo "building zstdra in bin$ARCH"
 
 # print all executed commands to the terminal
 set -x
 
+# set up environment
+# ------------------
+
+# Deps
+# ----
+if [ ! -f /build/zstd-1.3.8/lib/libzstd.a ] ; then
+	source /hbb_shlib/activate
+	cd /build
+	curl -O -L https://github.com/facebook/zstd/releases/download/v1.3.8/zstd-1.3.8.tar.gz
+	tar xvzf zstd-1.3.8.tar.gz
+	cd zstd-1.3.8
+	make
+	source /hbb_exe/activate
+fi
+
+if [ ! -f /build/zstdmt-0.6/programs/zstd-mt ] ; then
+	source /hbb_shlib/activate
+	cd /build
+	curl -o zstdmt-0.6.tar.gz -L https://github.com/mcmilk/zstdmt/archive/0.6.tar.gz
+	tar xvzf zstdmt-0.6.tar.gz
+	cd /build/zstdmt-0.6/programs
+	make loadsource
+	make zstd-mt
+	source /hbb_exe/activate
+fi
+
+cp /build/zstdmt-0.6/programs/zstd-mt /io/bin$ARCH
+
+# Build
+# -----
+
+echo "building zstd-$arch"
+
 # Compile
 cd /io/src
-make clean
-make
+if [ "$clean" = 1 ] ; then
+	make clean
+fi
 
-echo "Finished building zstdra and zstdindex"
+CPATH="/build/zstd-1.3.8/lib:/build/zstd-1.3.8/lib/decompress:/build/zstd-1.3.8/programs:$CPATH" LIBRARY_PATH="/build/zstd-1.3.8/lib:$LIBRARY_PATH" make
+
+echo "Finished building zstdra"
